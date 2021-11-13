8ccdd62f7604fb504445a4af572d5b6b3363161e
Michael Huso <husomichael@gmail.com>
2021-11-12 19:56:38 -0600

    made takeUserInputs function and setArithmetic function to set arithmetic value based on buttons clicked, and push number 1, arithmetic, number 2 into array inside userInputs object. Ready to make post and get routes.

--- server/public/client.js
+++ server/public/client.js
@@ -2,8 +2,17 @@ console.log('js');
 
 $(document).ready(readyNow);
 
+let userInputs = {inputs:[]};
+let arithmetic;
+
 function readyNow(){
     console.log('jq');
+    $('#plus-button').on('click', {arithmetic: 'plus'}, setArithmetic);
+    $('#minus-button').on('click', {arithmetic: 'minus'}, setArithmetic);
+    $('#multiply-button').on('click', {arithmetic: 'multiply'}, setArithmetic);
+    $('#divide-button').on('click', {arithmetic: 'divide'}, setArithmetic);
+    $('#equals-button').on('click', takeUserInputs);
+    $('#clear-button').on('click', takeUserInputs);
 ;}
 
 // Take user inputs and when submit button is clicked bundle them into an object
@@ -11,3 +20,14 @@ function readyNow(){
 
 // C button should clear user inputs.
 
+function takeUserInputs(){
+    userInputs.inputs.push($('#input-number-1').val());
+    userInputs.inputs.push(arithmetic);
+    userInputs.inputs.push($('#input-number-2').val());
+    console.log(userInputs.inputs);
+}
+
+function setArithmetic(event){
+    console.log(event.data.arithmetic);
+    arithmetic = event.data.arithmetic;
+}
